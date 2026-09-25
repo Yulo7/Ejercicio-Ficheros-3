4022ebe2c18919ce196c989760ca1a278da4a284
SergioColoméY <s.colomeyuste@gmail.com>
2026-09-25 09:21:28 +0200

    pene

--- src/Ampliacion/Ejercicio5.java
+++ src/Ampliacion/Ejercicio5.java
@@ -34,7 +34,7 @@ public class Ejercicio5 {
             System.out.println("Contiene " + contador + " lineas");
             //Orden Inverso
             for (int i = contador - 1; i >= 0; i--) {
-                try (BufferedWriter escribir= new BufferedWriter(new FileWriter(ruta+"/salida.txt"));){
+                try (BufferedWriter escribir= new BufferedWriter(new FileWriter(ruta+"/salida.txt",true));){
                     escribir.write(orden[i]);
                     escribir.newLine();
                 }
